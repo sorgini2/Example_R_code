4e83d141433862cf5cfedaaac650c527b599dcbc
sorgini2 <49991478+sorgini2@users.noreply.github.com>
2019-04-25 13:00:39 -0400

    Update hap_to_numeric.R

--- hap_to_numeric.R
+++ hap_to_numeric.R
@@ -1,5 +1,4 @@
 #Takes hampmap file and produces an output file after appling maf,het filters
-#Shows genomic region responsible for YG3 mutant phenotype
 setwd("C:/Users/sorgini2/Desktop/hap_to_num")
 directory="C:/Users/sorgini2/Desktop/hap_to_num/"
 file_list1=list.files(directory)
